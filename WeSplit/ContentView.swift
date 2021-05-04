7021f53eb344cc6c7f9ee98196b1f186c189826b
Alex Babou <ababou@dendev.net>
2021-05-03 23:58:44 -0600

    Initial Commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -2,15 +2,55 @@
 //  ContentView.swift
 //  WeSplit
 //
-//  Created by Alex on 5/3/21.
+//  Created by Alex Babou on 5/2/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercent = 2
+    
+    let tipOptions = [0, 10, 15, 20, 25]
+    
+    var calculatedCost: Double {
+        let people = Double(numberOfPeople + 2)
+        let tip = Double(tipOptions[tipPercent]) / 100
+        let amount = Double(checkAmount) ?? 0
+        
+        return amount * (1 + tip) / people
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Details:")) {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach (2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("Input your tip amount below:")) {
+                    Picker("Percent Tip", selection: $tipPercent) {
+                        ForEach(0 ..< tipOptions.count) {
+                            Text("\(self.tipOptions[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Result:")) {
+                    Text("A total of \(calculatedCost, specifier: "$%.2f") is split between \(numberOfPeople + 2) people.")
+                        .font(.headline)
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
